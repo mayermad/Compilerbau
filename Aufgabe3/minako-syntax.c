d53f325dbf18cde15ab9097b18f50fea1b12a1aa
Jonitr0 <jonitro165@gmail.com>
2020-06-10 22:18:00 +0200

    parser working

--- Aufgabe3/minako-syntax.c
+++ Aufgabe3/minako-syntax.c
@@ -6,9 +6,16 @@
 
 #define BUFFERSIZE 64
 
+int Debug = 0;
+
 void die(char* msg) {
    printf("%s\n", msg);
    exit(-1);
+}
+
+void debug(char* msg, const char* arg) {
+   if(Debug)
+      printf("%s%s\n", msg, arg);
 }
 
 //append b to a
@@ -20,7 +27,7 @@ char* append(char* a, char* b) {
 }
 
 char* inputString;
-int readChars = 0;
+int numberCharsRead = 0;
 
 void callLexer() {
    int token;
@@ -49,12 +56,13 @@ void callLexer() {
 //check if the next sequence of chars in the input String equals "toCheck"
 //returns 1 if the sequence matches, 0 if it doesn't
 int check(const char* toCheck) {
-   printf("checking %s\n", toCheck);
+   //debug("checking ", toCheck);
    return (strncmp(toCheck, inputString, strlen(toCheck))==0);
 }
 
 int checkInt(const int intToCheck) {
    char toCheck[16];
+   memset(toCheck, '\0', 16);
    sprintf(toCheck, "%d", intToCheck);
    return check(toCheck);
 }
@@ -62,11 +70,15 @@ int checkInt(const int intToCheck) {
 //terminate the programm with an error if the next sequence of chars does not equal "toEat", otherwise move the pointer on the input String
 void eat(char* toEat) {
    if(!check(toEat)) {
-      printf("token expected: %s after %d chars read\n", toEat, readChars);
+      char badToken[strlen(toEat)];
+      strncpy(badToken, inputString, strlen(toEat));
+      badToken[strlen(toEat)] = '\0';
+      printf("token expected: \"%s\", read this instead: \"%s\", %d chars read\n", toEat, badToken, numberCharsRead);
       die("Parser error");
    }
    inputString += strlen(toEat);
-   readChars += strlen(toEat);
+   numberCharsRead += strlen(toEat);
+   debug("ate ", toEat);
 }
 
 void eatInt(const int intToEat) {
@@ -81,6 +93,7 @@ typedef void metasym_f(void);
 metasym_f P, FD, FD2, FD3, FD4, Id, FC, SL, B, S, IF, R, A1, PR, TY, SA, A, E, E1, SE, SE1, SE2, SE3, SE4, T, T1, T2, T3, F;
 
 void P() {
+   debug("starting ", "P");
    FD(), eat("eof");
 }
 
@@ -90,9 +103,11 @@ int checkType() {
 }
 
 void FD() {
+   debug("starting ", "FD");
    while(checkType()) {
       TY(), eatInt(ID), eat("("), FD2(), eat(")"), FD3(), SL(), FD4();
    }
+   debug("completed ", "FD");
 }
 
 void FD2() {
@@ -127,10 +142,11 @@ void FD4() {
 
 //check if next symbol is in first-set of assignment
 int checkAssignment() {
-   return (checkInt(ID)||checkInt(KW_BOOLEAN)||checkInt(KW_INT)||checkInt(KW_FLOAT)||check("\"-\""));
+   return (checkInt(ID)||checkInt(CONST_BOOLEAN)||checkInt(CONST_INT)||checkInt(CONST_FLOAT)||check("-"));
 }
 
 void FC() {
+   debug("starting ", "FC");
    eatInt(ID), eat("(");
    //if there is at least one arg
    if(checkAssignment()) {
@@ -141,20 +157,24 @@ void FC() {
       eat(","), A();
    }
    eat(")");
+   debug("completed ", "FC");
 }
 
 //check if next symbol is in first-set of block
 int checkBlock() {
-   return (checkInt(ID)||checkInt(KW_BOOLEAN)||checkInt(KW_INT)||checkInt(KW_FLOAT)||check("-")||check("("));
+   return (checkInt(ID)||checkInt(KW_IF)||checkInt(KW_RETURN)||checkInt(KW_PRINTF)||checkInt(KW_BEGIN)||check("{"));
 }
 
 void SL() {
+   debug("starting ", "SL");
    while(checkBlock()) {
       B();
    }
+   debug("completed ", "SL");
 }
 
 void B() {
+   debug("starting ", "B");
    if(check("{"))
       eat("{");
    else if(checkInt(KW_BEGIN))
@@ -169,9 +189,11 @@ void B() {
       eat("}");
    else
       eatInt(KW_END);
+   debug("completed ", "B");
 }
 
 void S() {
+   debug("starting ", "S");
    //both function call and asignment start with an Id, check one step further
    char idAsign[16];
    sprintf(idAsign, "%d=", ID);
@@ -190,15 +212,160 @@ void S() {
    //fuction call
    else
       FC(), eat(";");
+   debug("completed ", "S");
 }
 
-void TY() {}
-void A() {}
+void IF() {
+   debug("starting ", "IF");
+   eatInt(KW_IF), eat("("), A(), eat(")"), B();
+   debug("completed ", "IF");
+}
+
+void R() {
+   debug("starting ", "R");
+   eatInt(KW_RETURN);
+   //assignment after return statement is optional
+   if(checkAssignment())
+      A();
+   debug("completed ", "R");
+}
+
+void PR() {
+   debug("starting ", "PR");
+   eatInt(KW_PRINTF), eat("("), A(), eat(")");
+   debug("completed ", "PR");
+}
+
+void TY() {
+   debug("starting ", "TY");
+   if(checkInt(KW_BOOLEAN))
+      eatInt(KW_BOOLEAN);
+   else if(checkInt(KW_INT))
+      eatInt(KW_INT);
+   else if(checkInt(KW_FLOAT))
+      eatInt(KW_FLOAT);
+   else
+      eatInt(KW_VOID);
+   debug("completed ", "TY");
+}
+
+void SA() {
+   debug("starting ", "SA");
+   eatInt(ID), eat("="), A();
+   debug("completed ", "SA");
+}
+
+void A() {
+   debug("starting ", "A");
+   //recursivly call A for further assignements
+   char idAsign[5];
+   sprintf(idAsign, "%d=", ID);
+   if(check(idAsign)) {
+      eatInt(ID), eat("="), A();
+   }
+   else {
+      E();
+   }
+   debug("completed ", "A");
+}
+
+void E() {
+   debug("starting ", "E");
+   SE();
+   if(checkInt(EQ)) {
+      eatInt(EQ), SE();
+   }
+   else if(checkInt(NEQ)) {
+      eatInt(NEQ), SE();
+   }
+   else if(checkInt(LEQ)) {
+      eatInt(LEQ), SE();
+   }
+   else if(checkInt(GEQ)) {
+      eatInt(GEQ), SE();
+   }
+   else if(checkInt(LSS)) {
+      eatInt(LSS), SE();
+   }
+   else if(checkInt(GRT)) {
+      eatInt(GRT), SE();
+   }
+   //nothing also possible
+   debug("completed ", "E");
+}
+
+void SE() {
+   debug("starting ", "SE");
+   if(check("-")) {
+      eat("-");
+   }
+   T();
+   while(1) {
+      if(check("+")) {
+         eat("+"), T();
+      }
+      else if(check("-")) {
+         eat("-"), T();
+      }
+      else if(checkInt(OR)) {
+         eatInt(OR), T();
+      }
+      else {
+         //no further terms
+         break;
+      }
+   }
+   debug("completed ", "SE");
+}
+
+void T() {
+   debug("starting ", "T");
+   F();
+   while(1) {
+      if(check("*")) {
+         eat("*"), F();
+      }
+      else if(check("/")) {
+         eat("/"), F();
+      }
+      else if(checkInt(AND)) {
+         eatInt(AND), F();
+      }
+      else {
+         //no further factors
+         break;
+      }
+   }
+   debug("completed ", "T");
+}
+
+void F() {
+   debug("starting ", "F");
+   char fc[5];
+   sprintf(fc, "%d(", ID);
+
+   if(checkInt(CONST_BOOLEAN))
+      eatInt(CONST_BOOLEAN);
+   else if(checkInt(CONST_INT))
+      eatInt(CONST_INT);
+   else if(checkInt(CONST_FLOAT))
+      eatInt(CONST_FLOAT);
+   else if(check(fc)) {
+      FC();
+   }
+   else if(checkInt(ID))
+      eatInt(ID);
+   else {
+      eat("("), A(), eat(")");
+   }
+   debug("completed ", "F");
+}
 
 //check if the grammar of the input string is valid
 void parseInput() {
-   printf("%s\n", inputString);
+   debug("input: ", inputString);
    P();
+   printf("Parsing complete, no errors detected.\n");
 }
 
 int main(int argc, char* argv[]) {
